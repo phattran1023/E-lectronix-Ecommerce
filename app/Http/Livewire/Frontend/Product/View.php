864a62fa02491911e5b10c0b3fe9bd32253a8a49
phattran1023 <ptrnvnh@gmail.com>
2023-07-26 23:29:18 +0700

    Add product to wishlist

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -2,13 +2,37 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Wishlist;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class View extends Component
 {
     public $category, $product, $prodColorSelectedQuantity;
 
-    public function colorSelected ($productColorId) 
+    public function addToWishList($productId)
+    {
+        // dd($productId);
+        if (Auth::check()) {
+            if (Wishlist::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists()) 
+            {
+                session()->flash('message', 'Your product has already been added to wishlist'); 
+                return false;
+            } else {
+                Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId
+                ]);
+                session()->flash('message', 'Added to Wishlist'); 
+
+            }
+        } else {
+            session()->flash('message', 'Please login to continue');
+            return false;
+        }
+    }
+
+    public function colorSelected($productColorId)
     {
         // dd($productColorId);
         $productColor = $this->product->productColors()->where('id', $productColorId)->first();
@@ -23,12 +47,11 @@ class View extends Component
     {
         $this->category = $category;
         $this->product = $product;
-
     }
 
     public function render()
     {
-        return view('livewire.frontend.product.view',[
+        return view('livewire.frontend.product.view', [
             'category' => $this->category,
             'product' => $this->product
         ]);
--- resources/views/livewire/frontend/product/view.blade.php
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -1,6 +1,11 @@
 <div>
     <div class="py-3 py-md-5 ">
         <div class="container">
+            @if (session()->has('message'))
+                <div class="alert alert-warning">
+                    {{ session('message') }}
+                </div>
+            @endif
             <div class="row">
                 <div class="col-md-5 mt-3">
                     <div class="bg-white border">
@@ -65,31 +70,38 @@
                         </div>
                         <div class="mt-2">
                             <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
-                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> Add To Wishlist </a>
+                            <button type="button" wire:click="addToWishList( {{ $product->id }} )" class="btn btn1">
+                                <span wire:loading.remove>
+                                    <i class="fa fa-heart"></i> Add To Wishlist
+                                </span>
+                                <span wire:loading wire:target="addToWishList">Adding ... </span>
+                            </button>
+
                         </div>
-                        <div class="mt-3">
-                            <h5 class="mb-0">Small Description</h5>
-                            <p>
-                                {!! $product->small_description !!}
-                            </p>
-                        </div>
+                    </div>
+                    <div class="mt-3">
+                        <h5 class="mb-0">Small Description</h5>
+                        <p>
+                            {!! $product->small_description !!}
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 mt-3">
-                    <div class="card">
-                        <div class="card-header bg-white">
-                            <h4>Description</h4>
-                        </div>
-                        <div class="card-body">
-                            <p>
-                                {!! $product->description !!}
-                            </p>
-                        </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 mt-3">
+                <div class="card">
+                    <div class="card-header bg-white">
+                        <h4>Description</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            {!! $product->description !!}
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
