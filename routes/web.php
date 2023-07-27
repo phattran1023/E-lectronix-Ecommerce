39024db8497e68a40a30a99996961938f74472c9
phattran1023 <ptrnvnh@gmail.com>
2023-07-27 09:08:47 +0700

    Improve functionality of wishlist

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::get('/collections',[App\Http\Controllers\Frontend\FrontendController::cla
 Route::get('collections/{category_slug}',[App\Http\Controllers\Frontend\FrontendController::class,'products']);
 Route::get('collections/{category_slug}/{product_slug}',[App\Http\Controllers\Frontend\FrontendController::class,'productView']);
 
+Route::get('wishlist',[App\Http\Controllers\Frontend\WishlistController::class,'index']);
+
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
