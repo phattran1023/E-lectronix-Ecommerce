5ad5002a1b9cc3959f2e45ede3cc4ab6b7f9bfb7
phattran1023 <ptrnvnh@gmail.com>
2023-08-02 11:48:00 +0700

    Added display for order's items and order's details

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::middleware(['auth'])->group(function() {
     Route::get('wishlist',[App\Http\Controllers\Frontend\WishlistController::class,'index']);
     Route::get('cart',[App\Http\Controllers\Frontend\CartController::class,'index']);
     Route::get('checkout',[App\Http\Controllers\Frontend\CheckoutController::class,'index']);
+    Route::get('orders',[App\Http\Controllers\Frontend\OrderController::class,'index']);
+    Route::get('orders/{orderId}',[App\Http\Controllers\Frontend\OrderController::class,'show']);
+
 
 });
 
