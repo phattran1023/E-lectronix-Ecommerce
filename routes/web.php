2af8dd7292f274c1c283673ad98bc5d2ef5c55a5
phattran1023 <ptrnvnh@gmail.com>
2023-08-02 21:21:52 +0700

    Added Update Order's Status function in admin panel

--- routes/web.php
+++ routes/web.php
@@ -104,6 +104,7 @@ Route::prefix('admin')->middleware('auth', 'isAdmin')->group(function () {
     Route::controller(App\Http\Controllers\Admin\OrderController::class)->group(function () {
         Route::get('/orders', 'index');
         Route::get('/orders/{orderId}', 'show');
+        Route::put('/orders/{orderId}', 'updateOrderStatus');
         
     });
 });
