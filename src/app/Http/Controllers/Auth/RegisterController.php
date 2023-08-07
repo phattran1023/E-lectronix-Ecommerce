5dbe6097bb844dad1dec2178179f20bf35f796e8
phattran1023 <ptrnvnh@gmail.com>
2023-08-07 18:22:33 +0700

    Add cenorship for Register form (Name field)

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,9 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\File;
 
 class RegisterController extends Controller
 {
@@ -49,11 +52,53 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        // Read the CSV file and get bad words
+        $badWords = $this->getBadWords();
+
+        // Define the custom validation rule for bad words
+        $badWordRule = Rule::notIn($badWords);
+
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                $badWordRule, // Use the custom validation rule for name
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                'unique:users',
+                $badWordRule, // Use the custom validation rule for email
+            ],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ], [
+            // Custom error message for the bad word validation rule
+            'name.not_in' => 'The :attribute contains sensitive words.',
+            'email.not_in' => 'The :attribute contains sensitive words.',
         ]);
+    }
+
+    // Helper method to read bad words from the CSV file
+    protected function getBadWords()
+    {
+        $path = public_path('badWords.csv');
+
+        if (File::exists($path)) {
+            $badWords = [];
+            if (($handle = fopen($path, 'r')) !== false) {
+                while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+                    $badWords[] = $data[0]; // Assuming the bad words are in the first column of the CSV file
+                }
+                fclose($handle);
+            }
+            return $badWords;
+        } else {
+            // Return an empty array if the file is not found
+            return [];
+        }
     }
 
     /**
