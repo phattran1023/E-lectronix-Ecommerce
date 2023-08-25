d7e9ee69ce7504888f0348ff593f6bda5f723c8f
anh tien <anhtienb237@gmail.com>
2023-08-25 21:20:42 +0700

    updating report(85% done)

--- src/app/Http/Controllers/Frontend/ReportedComment.php
+++ src/app/Http/Controllers/Frontend/ReportedComment.php
@@ -3,38 +3,50 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Comment;
-use App\Models\ReportedComment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
 
-class WishlistController extends Controller
+use App\Models\Comment;
+use App\Models\ReportComment;
+
+use Illuminate\Support\Facades\Validator;
+
+
+class ReportedComment extends Controller
 {
-   public function store(Request $request){
+ 
+   public function store(Request $request,$id){
     $validator = Validator::make($request->all(), [
-        'form-violence'=>'nullable',
-        'form-hate'=>'nullable',
-        'form-suicide'=>'nullable',
-        'form-misinformation'=>'nullable',
-        'form-frauds'=>'nullable',
-        'form-deceptive'=>'nullable',
+        'form_violence'=>'nullable',
+        'form_hate'=>'nullable',
+        'form_suicide'=>'nullable',
+        'form_misinformation'=>'nullable',
+        'form_frauds'=>'nullable',
+        'form_deceptive'=>'nullable',
        
         'form-else'=>'nullable|max:50',
+        'commentInfo'=> 'nullable',
     ]);
 
     if ($validator) {
       //handle success
-        $commentInfo = Comment::where('id','commentInfo');
-      $reportComment = new ReportedComment();
-      $reportComment->;
-
+    
+    $commentInfo = Comment::findOrFail($id);
+      $reportComment = new ReportComment();
+      $reportComment->report_id = $commentInfo->id;
+      $reportComment->reporter_id = $commentInfo->user_id;
+      $reportComment->user_comment = $commentInfo->comment_body;
+      $reportComment->violence = $request->form_violence;
+      $reportComment->hate = $request->form_hate;
+      $reportComment->suicide = $request->form_suicide;
+      $reportComment->misinformation = $request->form_misinformation;
+      $reportComment->frauds = $request->form_frauds;
+      $reportComment->deceptive = $request->form_deceptive;
+      $reportComment->else = $request->form_else;
+      $reportComment->save();
+      return redirect()->back()->with('reportComment', 'Reported successfully, wait for response.');
     } else {
         //handle fail
-        return response()->json([
-            'status' => 500,
-            'elseComment' => 'Words must be less then 30',
-        ]);
+        return redirect()->back()->with('reportComment', 'Something went wrong');
     }
        
 
